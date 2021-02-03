19dbfd74015101c314659a2fa999f55cb098ea6a
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2021-02-03 16:20:47 +0100

    Prettify

--- bloodbowlback/server.js
+++ bloodbowlback/server.js
@@ -2,19 +2,22 @@ const express = require("express");
 const app = express();
 const { db } = require("./conf");
 const port = 5050;
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
-
-app.get("/news", (req,res) =>{
-  db.query(`SELECT title, content_article FROM news_bb`, 
-  (error,result) =>{
-    if (error) {
-      return res.status(500).send("Error");
+app.get("/news", (req, res) => {
+  db.query(
+    `SELECT title_article, content_article, image_new FROM news_bb`,
+    (error, result) => {
+      if (error) {
+        return res.status(500).send("Error");
+      }
+      return res.json(result);
     }
-    return res.json(result)
-  })
-})
+  );
+});
 
-app.listen(port,()=>{
-  console.log(`Server is running on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
--- bloodbowlteam/src/styled-components/Sidebar.jsx
+++ bloodbowlteam/src/styled-components/Sidebar.jsx
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const DivSidebar = styled.div`
   width: 38vh;
-  height : 100%;
+  height: 100%;
   text-align: center;
   background-color: #2e3654;
 `;
 
 export const P = styled.p`
   padding-top: 16vh;
-  padding-left : 1vh;
+  padding-left: 1vh;
   padding-right: 1vh;
   color: white;
   font-size: 25px;
