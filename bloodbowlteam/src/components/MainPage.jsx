c7c5b8a806bbaea1a8f4981ecedb239074b99538
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2021-02-03 16:07:12 +0100

    Add news and image

--- bloodbowlteam/src/components/MainPage.jsx
+++ bloodbowlteam/src/components/MainPage.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  DivMain,
+  DivNew,
+  DivNewContain,
+  H2,
+  Img,
+  P,
+} from "../styled-components/MainPage";
+import Sidebar from "./Sidebar";
 
 export default function MainPage() {
   const [articles, setArticles] = useState([]);
@@ -18,18 +27,26 @@ export default function MainPage() {
   }, [setArticles]);
 
   return (
-    <div>
-      {loading && <p>Loading</p>}
-      {!loading && (
-        <>
-          {articles.map((article, i) => (
-            <div key={i}>
-              <h2>{article.title_article}</h2>
-              <p>{article.content_article}</p>
-            </div>
-          ))}
-        </>
-      )}
-    </div>
+    <DivMain>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        {loading && <p>Loading</p>}
+        {!loading && (
+          <>
+            {articles.map((article, i) => (
+              <DivNew key={i}>
+                <H2>{article.title_article}</H2>
+                <DivNewContain>
+                  <Img src={article.image_new} alt="image" />
+                  <P>{article.content_article}</P>
+                </DivNewContain>
+              </DivNew>
+            ))}
+          </>
+        )}
+      </div>
+    </DivMain>
   );
 }
---
+++ bloodbowlteam/src/styled-components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const DivSidebar = styled.div`
+  width: 36vh;
+  text-align: center;
+  background-color: #2e3654;
+`;
+
+export const P = styled.p`
+  padding-top: 16vh;
+  color: white;
+  font-size: 25px;
+`;
+
+export const Img = styled.img`
+  width: 35vh;
+  padding-top: 57vh;
+`;
